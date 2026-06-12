1d8a0dfb3d6d7d810b7ae452749d7c5a3bee28d4
Jai Jayesh Shah <81829486+alexmercel@users.noreply.github.com>
2026-06-12 10:05:20 -0700

    Add: last-stone-weight - submission-8

---
+++ Data Structures & Algorithms/last-stone-weight/submission-8.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        arr = sorted(stones)
+        while len(arr)>1:
+            if arr[-1]==arr[-2]:
+                arr=[0]+arr[:-2]
+                
+            else :
+                arr=arr[:-2]+[(arr[-1]-arr[-2])]
+                arr.sort()
+        return arr[0]
+
+        
